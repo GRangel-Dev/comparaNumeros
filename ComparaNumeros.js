5c64876d310bab82fd636b85da2fbf0be48de1f9
GRangel-Dev <rantro.dev@gmail.com>
2022-05-27 17:02:20 -0300

    alteração na escrita de acordo com as boas práticas

--- ComparaNumeros.js
+++ ComparaNumeros.js
@@ -1,12 +1,12 @@
 function comparaNumeros(n1, n2) {
      if(!n1 || !n2) return `Defina dois números`;
-	const frase1 = PrimeiraFrase(n1, n2);
-	const frase2 = SegundaFrase(n1, n2);
+	const frase1 = PRIMEIRA_FRASE(n1, n2);
+	const frase2 = SEGUNDA_FRASE(n1, n2);
 
 	return `${frase1} ${frase2}`;
 }
 
-function PrimeiraFrase(n1, n2) {
+function PRIMEIRA_FRASE(n1, n2) {
 	let simOuNao = '';
 
 	if (n1 !== n2) {
@@ -16,7 +16,7 @@ function PrimeiraFrase(n1, n2) {
 	return `Os números ${n1} e ${n2} ${simOuNao} são iguais.`;
 }
 
-function SegundaFrase(n1, n2) {
+function SEGUNDA_FRASE(n1, n2) {
 	const soma = n1 + n2;
 
 	let resultDez = 'menor';
